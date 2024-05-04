a62b451c2b9184b2e23e4909a29772b53ceda65b
Pour1a-D <pour1a.dra80@gmail.com>
2024-05-04 14:25:52 +0330

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import App from "./App.tsx";
 import ReactDOM from "react-dom/client";
 
+import { BrowserRouter } from "react-router-dom";
+
 import "./assets/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/router/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+    HOME: "/",
+    NOT_FOUND_ROUTE: "/*",
+};
